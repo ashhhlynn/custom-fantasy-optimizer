7bb327edc642fb2cb2b60b186d52e7893a578a08
ashhhlynn <ashleylynnwelch@gmail.com>
2025-06-28 04:15:02 -0500

    Add pulp optimization solver

--- dfs.py
+++ dfs.py
@@ -1,12 +1,13 @@
 import requests
 import json
+from pulp import *
 
 def fetch_sleeper_players():
-    # fetch sleeper API
+    # Fetching sleeper API
     sleeper_API = requests.get('https://api.sleeper.app/projections/nfl/2023/18?season_type=regular&position%5B%5D=DEF&position%5B%5D=K&position%5B%5D=RB&position%5B%5D=QB&position%5B%5D=TE&position%5B%5D=WR&order_by=ppr')
     sleeper_data = sleeper_API.text 
     json_sleeper_data = json.loads(sleeper_data)
-    # list players with name and projection keys
+    # Pulling player names and projections
     sleeper_players = []
     for item in json_sleeper_data:
         projection = item['stats'].get('pts_ppr')
@@ -17,28 +18,53 @@ def fetch_sleeper_players():
     return(sleeper_players)
 
 def fetch_dk_players(): 
-    # fetch draftkings API 
+    # Fetching draftkings API 
     dk_API = requests.get('https://api.draftkings.com/draftgroups/v1/draftgroups/98582/draftables')
     dk_data = dk_API.text 
     json_dk_data = json.loads(dk_data)
-    sleeper_players = fetch_sleeper_players()
+    sleeper_players = fetch_sleeper_players()    
     dk_players = []
-    dk_players_full = []   
+    salaries = {'QB':{}, 'RB':{}, 'WR':{}, 'TE':{}, 'FLEX':{}, 'DST':{}}
+    projections = {'QB':{}, 'RB':{}, 'WR':{}, 'TE':{}, 'FLEX':{}, 'DST':{}}
     for index, item in enumerate(json_dk_data['draftables']):
         if item['draftStatAttributes'][0].get('id') == 90:                
-            if index != len(json_dk_data['draftables'])-1 and item['playerId'] == json_dk_data['draftables'][index + 1]['playerId']:
-                # find matching sleeper player projection
+            if index != len(json_dk_data['draftables'])-1 and item['playerId'] != json_dk_data['draftables'][index + 1]['playerId']:
+                # Finding matching sleeper player projection
                 for m in sleeper_players:
                     if item['displayName'] in m:
-                        projection = m[item['displayName']] 
-                        # add player with position key 
-                        info = {index: 1, item['position']: 1, 'projection': projection}
-                        new_player = {**item, **info}                
-                        dk_players.append(new_player)
-                        dk_players_full.append(new_player)
-                        # add duplicate player with flex key 
-                        flex_info = {index+1000: 1, 'FLEX':1, item['position']:0, 'projection': projection}
-                        new_flex = {**item, **flex_info}
-                        dk_players_full.append(new_flex)
-    print(dk_players)
-fetch_dk_players()      
+                        # Adding player to all players, salaries, and projections 
+                        info = {'name': item['displayName'], 'position': item['position'], 'salary': item['salary'], 'projection': m[item['displayName']]}
+                        salaries[item['position']].update({item['displayName']: item['salary']})
+                        projections[item['position']].update({item['displayName']: m[item['displayName']]})
+                        dk_players.append(info)
+                        # Adding flex player 
+                        if item['position'] in ('RB', 'WR', 'TE'):
+                            salaries['FLEX'].update({item['displayName']: item['salary']})
+                            projections['FLEX'].update({item['displayName']: m[item['displayName']]})
+    # Setting position and salary constraints
+    pos_max = {
+        'QB': 1,
+        'RB': 2,
+        'WR': 3,
+        'TE': 1,
+        'FLEX': 1,
+        'DST': 1
+    }
+    salary_max = 50000
+    # Setting up and solving problem 
+    prob = LpProblem("Optimize DFS", LpMaximize)
+    _vars = {k: LpVariable.dict(k, v, cat="Binary") for k, v in projections.items()}    
+    points = []
+    prices = []
+    for k, v in _vars.items():
+        prices += lpSum([salaries[k][i] * _vars[k][i] for i in v])
+        points += lpSum([projections[k][i] * _vars[k][i] for i in v])
+        prob += lpSum([_vars[k][i] for i in v]) <= pos_max[k]
+    prob += lpSum(points)
+    prob += lpSum(prices) <= salary_max
+    prob.solve()
+    for v in prob.variables():
+        if v.varValue == 1:
+            print(f"{v.name} = {v.varValue}")
+
+fetch_dk_players()
