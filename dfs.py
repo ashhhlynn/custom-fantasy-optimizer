3a437514dff6b2bd8a737ab4884171411662707b
ashhhlynn <ashleylynnwelch@gmail.com>
2025-07-22 01:37:26 -0500

    Update options to include stacking QB/RB, QB/WR, QB/TE, and DST/RB

--- dfs.py
+++ dfs.py
@@ -35,7 +35,7 @@ def fetch_dk_players():
                     dk_players.update({str(index): {'name': item['displayName'], 'position': item['position'], 'team': item['teamAbbreviation'], 'opp': opp, 'salary': item['salary'], 'projection': sleeper_players[item['displayName'][:15]]}})
     return dk_players
 
-def optimize_dk_players(flex_input, incl_input, excl_input, dst_stack_input, qb_stack_input):
+def optimize_dk_players(flex_input, incl_input, excl_input, qb_stack_input, dst_stack_input):
     dk_players = fetch_dk_players()  
     # Define PuLP problem and variable. 
     prob = LpProblem('Optimize', LpMaximize)
@@ -66,7 +66,7 @@ def optimize_dk_players(flex_input, incl_input, excl_input, dst_stack_input, qb_
         elif flex_input in ["RB", "WR", "TE"] and pos in ["RB", "WR", "TE"]:
             prob += lpSum([player_vars[p] for p in dk_players if dk_players[p]['position'] == pos]) == max_count - 1   
     # Define PuLP constraints for maximum players per team.  
-    team_constraints(dk_players, player_vars, prob, dst_stack_input, qb_stack_input)
+    team_constraints(dk_players, player_vars, prob, qb_stack_input, dst_stack_input)
     # Define PuLP objective to maximize total projection and solve. 
     prob += lpSum(dk_players[p]["projection"] * player_vars[p] for p in dk_players)
     prob.solve()
@@ -87,33 +87,38 @@ def optimize_dk_players(flex_input, incl_input, excl_input, dst_stack_input, qb_
     print("Total Projection:", pulp.value(prob.objective))
     print("Remaining Salary:", 50000 - sum(dk_players[p]["salary"] * player_vars[p].varValue for p in dk_players))
 
-def team_constraints(dk_players, player_vars, prob, dst_stack_input, qb_stack_input):
+def team_constraints(dk_players, player_vars, prob, qb_stack_input, dst_stack_input):
     teams = {}
     for data in dk_players.values():
         teams.update({data['team']: {'RB':0, 'WR':0, 'TE':0}}) if data["position"] == 'DST' else None
     for team in teams: 
-        # Require QB and at least 1 flex from the same team.
-        if qb_stack_input == 1:
-            flex = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] in ["RB", "WR", "TE"]])  
+        # Require QB + RB, WR, and/or TE from the same team if specified.
+        for pos in qb_stack_input:
+            flex = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] == pos])  
             qb = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] == "QB"])
             prob += lpSum(flex) >= lpSum(qb)
-        # Require exclusion of teams opposing the defense if specified.  
-        if dst_stack_input == 1:
+        # Require DST + RB from the same team if specified. 
+        if dst_stack_input[0] == 1:
+            rb = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] == 'RB'])  
+            dst = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] == "DST"])
+            prob += lpSum(rb) >= lpSum(dst)
+        # Require exclusion of teams opposing DST if specified.  
+        if dst_stack_input[1] == 1:
             other = lpSum([player_vars[k] for k in dk_players if dk_players[k]['opp'] == team and dk_players[k]['position'] != 'DST'])  
             dst = lpSum([player_vars[k] for k in dk_players if dk_players[k]['team'] == team and dk_players[k]['position'] == 'DST'])
             prob += lpSum(lpSum(other)) if lpSum(dst) >= 1 else None == 0
 
 # Option to require specific position for flex.
-flex_input = 'RB'
+flex_input = ''
 
 # Option to require inclusion or exclusion of specific players. 
 incl_input = ['111']
 excl_input = ['61']
 
-# Option to require qb and flex stack from the same team.
-qb_stack_input = 0
+# Option to require QB + RB, WR, and/or TE stacks from the same team.
+qb_stack_input = ['WR']
 
-# Option to require exclusion of teams opposing the defense. 
-dst_stack_input = 1
+# Options to require DST + RB stack from the same team and exclusion of teams opposing DST. 
+dst_stack_input = [0,0]
 
-optimize_dk_players(flex_input, incl_input, excl_input, dst_stack_input, qb_stack_input)
+optimize_dk_players(flex_input, incl_input, excl_input, qb_stack_input, dst_stack_input)
